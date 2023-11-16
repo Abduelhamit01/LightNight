53910389599ee8c80d3f4233ac27997d1adb4919
Abduelhamit <abduelhamit.o@gmail.com>
2023-11-16 21:47:39 +0100

    Pushhh

--- Light/ViewController.swift
+++ Light/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var lightOn = true
+    
     override func viewDidLoad() {
+
         super.viewDidLoad()
+        updateUI()
         // Do any additional setup after loading the view.
     }
+    
+     func updateUI() {
+         view.backgroundColor = lightOn ? .white : .black
+    }
 
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        lightOn.toggle()
+        updateUI()
+    }
+    
 
 }
 
